e097acd1c4df2114a7de82268994b3349189a4a6
Anwesh Gangula <gangulaanwesh@gmail.com>
2023-01-04 16:08:55 +0530

    MVVM 0 - (02:50) - define prent classes for notifier + command

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # WPF MVVM UI
 
-Reference: [WPF UI/ Design a Modern Login Form/ Introducing to WPF](https://www.youtube.com/watch?v=pZGcRHgmn8k)
+Reference: 
+- UI Code - [WPF UI/ Design a Modern Login Form/ Introducing to WPF](https://www.youtube.com/watch?v=pZGcRHgmn8k)
+- MVVM
+    - [Create Login in WPF, MVVM Pattern, C# and SQL Server - Step by Step + Display user data](https://www.youtube.com/watch?v=FGqj4q09NtA&list=PLwG-AtjFaHdO-irz58rJ6RI-9IrBIWf6v&index=1)
---
+++ WPF MVVM UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_UI.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertychanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
